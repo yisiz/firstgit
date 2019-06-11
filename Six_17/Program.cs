44a359c5bcd34b09f71f7ed18590b23958ff51a5
Arden Zhu <arden.zhu@lafayette148.com>
2019-06-10 23:53:39 -0400

    Update Code of Six_17: 1. rename variable commonDivisor 2. improve the algorithm

--- Six_17/Program.cs
+++ Six_17/Program.cs
@@ -10,18 +10,11 @@ namespace Six_17
             int a = int.Parse(Console.ReadLine());
             Console.Write("b = ");
             int b = int.Parse(Console.ReadLine());
-            int common_divisor = 1;
-            int i = 1;
-            while (i <= a )
-            {
-                if (a % i == 0 && b % i == 0)
-                {
-                    common_divisor = i;
-                }
-                i++;
-            }
-            Console.WriteLine(common_divisor);
-            Console.WriteLine(a * b / common_divisor);
+            int commonDivisor = Math.Min(a, b);
+            while (!(a % commonDivisor == 0 && b % commonDivisor == 0))
+                commonDivisor--;
+            Console.WriteLine(commonDivisor);
+            Console.WriteLine(a * b / commonDivisor);
         }
     }
 }
