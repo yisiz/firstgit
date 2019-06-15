d681c92d6b803d2a7b5b4737a0a00f79a56cb78f
Arden Zhu <arden.zhu@lafayette148.com>
2019-06-14 23:05:08 -0400

    NQueensRecursion

---
+++ LaoPig.NQueens/NQueensRecursion.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LaoPig.NQueens
+{
+    internal class NQueensRecursion
+    {
+        private readonly int n;
+        private int[] state;
+        private int count;
+
+        public NQueensRecursion(int n)
+        {
+            this.n = n;
+            this.state = new int[n];
+            this.count = 0;
+        }
+
+        public void Solve()
+        {
+            SolveRecursion(n - 1);
+        }
+
+        private void SolveRecursion(int c)
+        {
+            if (c < 0)
+            {
+                PrintASolution();
+            }
+            else
+            {
+                for (int i = n * c; i < n * c + n; i++)
+                {
+                    state[c] = i;
+                    if (test(c))
+                    {
+                        SolveRecursion(c - 1);
+                    }
+                }
+            }
+        }
+
+        // check if the queen at position pos fits all the others state[pos] vs state[pos + 1:]
+        private bool test(int pos)
+        {
+            int x1 = state[pos] % n;
+            int y1 = state[pos] / n;
+            for (int pos2 = pos + 1; pos2 < n; pos2++)
+            {
+                int x2 = state[pos2] % n;
+                int y2 = state[pos2] / n;
+                if (x1 == x2 || y1 == y2 || x1 - y1 == x2 - y2 || x1 + y1 == x2 + y2)
+                    return false;
+            }
+            return true;
+        }
+
+        public void PrintASolution()
+        {
+            Console.WriteLine("{0}: [{1}]", ++count, String.Join(',', state));
+        }
+
+    }
+}
---
+++ LaoPig.NQueens/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LaoPig.NQueens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            var nQueens = new NQueensRecursion(n);
+            nQueens.Solve();
+        }
+    }
+}
