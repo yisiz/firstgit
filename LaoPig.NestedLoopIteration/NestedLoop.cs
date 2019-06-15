ab801c29a51f8ecaf733efeacaadfccb427249d2
Arden Zhu <arden.zhu@lafayette148.com>
2019-06-14 23:27:38 -0400

    Nested Loop interator

---
+++ LaoPig.NestedLoopIteration/NestedLoop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LaoPig.NestedLoopIteration
+{
+    internal class NestedLoop
+    {
+        private readonly int n;
+        private readonly int k;
+        private int[] state;
+
+        public NestedLoop(int n, int k)
+        {
+            this.n = n;
+            this.k = k;
+            state = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                state[i] = -1;
+            }
+        }
+
+        internal void Enumerate()
+        {
+            int c = n - 1;
+            int count = 0;
+            while (c < n)
+            {
+                if (c < 0)
+                {
+                    Console.WriteLine("{0} [{1}]", ++count, string.Join(',', state));
+                    c++;
+                }
+                else
+                {
+                    if (state[c] + 1 < k)
+                    {
+                        state[c--] += 1;
+                    }
+                    else
+                    {
+                        state[c++] = -1;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LaoPig.NestedLoopIteration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LaoPig.NestedLoopIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int k = 2;
+            var nestedLoop = new NestedLoop(n, k);
+            nestedLoop.Enumerate();
+        }
+    }
+}
